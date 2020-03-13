8d3536db273dcac298b9307aa0fa11cfb6a3fd78
iamkjw77 <iamkjw77@naver.com>
2020-03-13 19:20:36 +0900

    up

---
+++ Level1/Q12918.js
@@ -0,0 +1,32 @@
+function solution(s) {
+    /*
+    var cnt = 0;
+    var arr = s.split('');
+    var arrType = new Array();
+    var num = /[0-9]/;
+
+    for(var key in arr){
+      if(arr.length == 4 || arr.length == 6){
+          arrType.push(num.test(arr[key]));
+      }else{
+        return false;
+      }
+    }
+
+    for(var key in arrType){
+      if(arrType[key] === false){
+        return false;
+      }else{
+        cnt += 1;
+      }
+    }
+
+    return cnt == 4 ? true : false;
+    */
+    var result = false;
+    if((s.length == 4 || s.length == 6) && /^[0-9]+$/.test(s)){
+      result = true;
+    }
+    return result;
+}
+console.log(solution('155505')); //true 출력
