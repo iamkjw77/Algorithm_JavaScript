351d74f428c7d970d8cdf9accb8d66ef8c0df0d5
iamkjw77 <iamkjw77@naver.com>
2020-04-12 01:11:42 +0900

    up

--- Level1/Q12982.js
+++ Level1/Q12982.js
@@ -1,4 +1,5 @@
 function solution(d, budget) {
+    /*
     var sum = 0;
     d.sort((a,b) => a-b);
     for(var i=0; i<d.length; i++){
@@ -10,6 +11,20 @@ function solution(d, budget) {
       }
     }
     if(sum < budget) return d.length;
+    */
 
+    /*
+    d.sort((a,b) => a-b);
+    while(d.reduce((acc,curr) => acc += curr, 0) > budget) d.pop();
+    return d.length;
+    */
+
+    var answer = 0;
+    var money = 0;
+    d.sort((a,b) => a-b).forEach(v => {
+      money += v;
+      if(money <= budget) answer++;
+    });
+    return answer;
 }
-console.log(solution([1,1,1,1],8))
+console.log(solution([1,1,1,1],8)) //4출력
