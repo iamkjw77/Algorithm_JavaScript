4f53c11244f56c25795ee6d7ecfe21cdf780e902
iamkjw77 <iamkjw77@naver.com>
2020-05-09 03:09:45 +0900

    up

--- Level1/Q64061.js
+++ Level1/Q64061.js
@@ -1,32 +1,23 @@
 function solution(board, moves) {
-    var answer = 0;
-    var arr = Array.from(board, v => Array());
-    var arr2 = [];
-
-    for(var i=0; i<board.length; i++){
-      for(var j=0; j<board.length; j++){
-        arr[i][j] = board[j][i];
-      }
-      arr[i] = arr[i].reverse();
-    }
-
+    var cnt = 0;
+    var arr = [];
 
     for(var i=0; i<moves.length; i++){
-      if(arr[moves[i]-1].pop() == 0){
-        arr[moves[i]-1].pop();
-      }else{
-        arr[moves[i]-1].pop();
+      for(var j=0; j<board.length; j++){
+        if(board[j][moves[i]-1] !== 0){
+          arr.push(board[j][moves[i]-1]);
+          board[j][moves[i]-1] = 0;
+          break;
+        }
       }
-
+      if(arr.length >= 2){
+        if(arr[arr.length-1] === arr[arr.length-2]){
+          arr.splice(arr.length-2,2);
+          cnt += 2;
+        }
+      }
     }
-    return arr;
-    /*board = board.map((v,i) => {
-      return v.map((val,idx) => {
-        return val = board[idx][i];
-      });
-      v.reverse();
-    });
-    */
-
+    return cnt;
 }
-console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]));
+
+console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])); // 4출력
