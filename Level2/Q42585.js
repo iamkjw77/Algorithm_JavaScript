e56d7538caeb5cb09ac6d7e7fb069fac3ddeeb25
iamkjw77 <iamkjw77@naver.com>
2020-05-14 00:42:08 +0900

    up

--- Level2/Q42585.js
+++ Level2/Q42585.js
@@ -1,38 +1,23 @@
 function solution(arrangement) {
-    var arr = arrangement.split('');
-    var queue = [];
-    var s = 0;
-    var e = [];
-    var cnt = 0;
+    var arr = arrangement.replace(/\(\)/g,'R');
+    var stack = [];
     var sum = 0;
 
+    for(var i=0; i<arr.length; i++){
+      switch(arr[i]){
+        case '(' :
+          stack.push(0);
+          break;
 
-    while(arr.length > 0){
-      var first = arr.shift();
-      if(first + arr[0] === '()'){
-        arr.shift();
-        queue.push('R');
-        continue;
-      }else{
-        queue.push(first);
+        case 'R' :
+          stack = stack.map(v => v+1);
+          break;
+
+        case ')' :
+          sum += stack.pop()+1;
+          break;
       }
     }
-
-    for(var i=0; i<queue.length; i++){
-      if(queue[i] === '('){
-        for(var j=i+1; j<queue.length; j++){
-          if(queue[j] == 'R'){
-            cnt++;
-          }else if(queue[j] === ')'){
-            queue.splice(j,1);
-            break;
-          }
-        }
-      }else{continue;}
-      sum += cnt+1;
-      cnt = 0;
-    }
-
     return sum;
 }
-console.log(solution("()(((()())(())()))(())"));
+console.log(solution("()(((()())(())()))(())")); //17출력
