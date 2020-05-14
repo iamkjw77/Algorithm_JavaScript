aa970ea90de42bdc63ccbe7adacd726920640edc
iamkjw77 <iamkjw77@naver.com>
2020-05-14 17:38:06 +0900

    up

--- Level2/Q42585.js
+++ Level2/Q42585.js
@@ -1,37 +1,4 @@
 function solution(arrangement) {
-<<<<<<< HEAD
-    var arr = arrangement.replace(/\(\)/g,'R').split('');
-    var queue = [];
-    var cnt = 0;
-    var sum = 0;
-
-    console.log(arr);
-    /*
-    while(arr.length > 0){
-      var first = arr.shift();
-      if(first + arr[0] === '()'){
-        arr.shift();
-        queue.push('R');
-        continue;
-      }else{
-        queue.push(first);
-      }
-    }
-    */
-    for(var i=0; i<arr.length; i++){
-      if(arr[i] === '('){
-        for(var j=i+1; j<arr.length; j++){
-          if(arr[j] === 'R'){
-            cnt++;
-          }else if(arr[j] === ')'){
-            arr.splice(j,1);
-            break;
-          }
-        }
-      }else{continue;}
-      sum += cnt+1;
-      cnt = 0;
-=======
     var arr = arrangement.replace(/\(\)/g,'R');
     var stack = [];
     var sum = 0;
@@ -50,7 +17,6 @@ function solution(arrangement) {
           sum += stack.pop()+1;
           break;
       }
->>>>>>> e56d7538caeb5cb09ac6d7e7fb069fac3ddeeb25
     }
     return sum;
 }
