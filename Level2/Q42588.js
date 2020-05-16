010d451717eb0a3afe44dbfc80dd8f9a8d6bb53c
iamkjw77 <iamkjw77@naver.com>
2020-05-17 01:40:49 +0900

    up

--- Level2/Q42588.js
+++ Level2/Q42588.js
@@ -1,16 +1,28 @@
 function solution(heights) {
+    /*
     var answer = [];
-    var firstEle = 0;
-    var a;
 
-    for(var i=heights.length-1 ; i>0; i--){
-      for(var j=i-1 ; j>0; j--){
-        if(heights[j] > heights[i]){
+    for(var i=0; i<heights.length; i++){
+      for(var j=i-1; j>=0; j--){
+        if(heights[i] < heights[j]){
           answer.push(j+1);
           break;
-        }
+        }else if(j==0) answer.push(0);
       }
     }
+
+    answer.unshift(0);
     return answer;
+    */
+
+    return heights.map((v,i) => {
+      while(i){
+        i--;
+        if(heights[i] > v){
+          return i+1;
+        }
+      }
+      return 0;
+    });
 }
-console.log(solution([6,9,5,7,4]));
+console.log(solution([6,9,5,7,4])); //[0,0,2,2,4] 출력
