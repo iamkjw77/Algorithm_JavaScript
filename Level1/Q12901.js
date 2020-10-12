e17a7098b692395151debf8533c264a86507f293
iamkjw77 <iamkjw77@naver.com>
2020-10-12 20:52:46 +0900

    up

--- Level1/Q12901.js
+++ Level1/Q12901.js
@@ -1,24 +1,17 @@
-/*
-function solution(a, b) {
-    var answer = '';
-    var week = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
-    var month = [31,29,31,30,31,30,31,31,30,31,30,31];
-    var sum = 0;
-    for(var i=0;i<a-1;i++){
-      sum += month[i];
-    }
-    sum += b-1;
-    var answer = week[sum%7];
-    return answer;
+// function solution(a, b) {
+//     let sum = 0;
+//     const days = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
+//     const month = [31,29,31,30,31,30,31,31,30,31,30,31];
+//
+//     for(let i=0; i<a-1; i++){
+//       sum += month[i];
+//     }
+//     sum += b-1;
+//     return days[sum%7];
+// }
+function solution(a, b){
+  const date = new Date(2016,a-1,b);
+  const week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+  return week[date.getDay()]; // date.getDay()는 숫자로 출력
 }
-*/
-
-function solution(a,b){
-  var answer = '';
-  var date = new Date(2016,a-1,b);
-  var week = ['SUN','MON','TUE','WED','THU','FRI','SAT']
-  return week[date.getDay()]; //date.getDay()는 숫자로 출력
-}
-
-/*테스트*/
-console.log(solution(3,2));
+console.log(solution(2,3));
