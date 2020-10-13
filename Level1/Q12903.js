d1d4ecedb804813f03bd4ead82332d42733e463a
iamkjw77 <iamkjw77@naver.com>
2020-10-13 19:49:46 +0900

    up

--- Level1/Q12903.js
+++ Level1/Q12903.js
@@ -1,17 +1,12 @@
-/*
 function solution(s) {
-    var words = new Array();
-    words = s.split('');
-    if(words.length%2 == 0){
-      return words[parseInt(words.length/2)-1]+words[parseInt(words.length/2)];
-    }else{
-      return words[parseInt(words.length/2)];
-    }
+    // const remain = s.length%2;
+    // const half = Math.floor(s.length/2);
+    //
+    // if(remain === 0){
+    //   return s.substr(half-1,2);
+    // }else{
+    //   return s.substr(half,1);
+    // }
+    return s.substr(Math.ceil(s.length/2)-1, s.length%2 === 0 ? 2 : 1);
 }
-*/
-
-function solution(s) {
-  return s.substr(Math.ceil(s.length/2)-1,s.length%2===0?2:1);
-}
-
-console.log(solution('asdf'));
+console.log(solution('avxldhd'));
