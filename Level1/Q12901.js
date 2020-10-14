cfd53f1a3d071146bbfe6a2bb71f5e6167abd293
iamkjw77 <iamkjw77@naver.com>
2020-10-14 11:02:09 +0900

    up

--- Level1/Q12901.js
+++ Level1/Q12901.js
@@ -1,17 +1,17 @@
-// function solution(a, b) {
-//     let sum = 0;
-//     const days = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
-//     const month = [31,29,31,30,31,30,31,31,30,31,30,31];
-//
-//     for(let i=0; i<a-1; i++){
-//       sum += month[i];
-//     }
-//     sum += b-1;
-//     return days[sum%7];
-// }
-function solution(a, b){
-  const date = new Date(2016,a-1,b);
-  const week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-  return week[date.getDay()]; // date.getDay()는 숫자로 출력
+function solution(a, b) {
+    let sum = 0;
+    const days = ['FRI','SAT','SUN','MON','TUE','WED','THU'];
+    const month = [31,29,31,30,31,30,31,31,30,31,30,31];
+
+    for(let i=0; i<a-1; i++){
+      sum += month[i];
+    }
+    sum += b-1;
+    return days[sum%7];
 }
+// function solution(a, b){
+//   const date = new Date(2016,a-1,b);
+//   const week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+//   return week[date.getDay()]; // date.getDay()는 숫자로 출력
+// }
 console.log(solution(2,3));
--- Level1/Q12906.js
+++ Level1/Q12906.js
@@ -1,25 +1,15 @@
-/*
 function solution(arr)
 {
-  var answer = [];
-  for(var i=0;i<arr.length;i++){
-    if(arr[i-1] != arr[i]){
-      answer.push(arr[i]);
-    }
-  }
-  return answer;
+  // const queue = [];
+  // queue.push(arr[0]);
+  //
+  // for(let i=1,j=0; i<arr.length; i++){
+  //   if(arr[i] !== queue[j]){
+  //     queue.push(arr[i]);
+  //     j++;
+  //   }
+  // }
+  // return queue;
+  return arr.filter((v,i) => v !== arr[i+1]);
 }
-*/
-
-/*
-function solution(arr){
-  return arr.filter(function(val,index){
-    return val != arr[index+1]; //요소값과 다음 인덱스의 요소값이 같지 않은 것만 리턴
-  });
-}
-*/
-function solution(arr){
-  return arr.filter((val,index) => val != arr[index+1]);
-}
-
-console.log(solution([4,4,4,3,3])); //[4,3]출력
+console.log(solution([1, 1, 3, 3, 0, 1, 1]));
