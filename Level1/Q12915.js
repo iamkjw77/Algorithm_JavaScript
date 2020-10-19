2e6dd1c3670fa16697c5a7df6867c9686e4d6317
jiwon <iamkjw77@naver.com>
2020-10-19 11:10:39 +0900

    refactor: 변수 선언 방식 변경

--- Level1/Q12915.js
+++ Level1/Q12915.js
@@ -1,17 +1,15 @@
 function solution(strings, n) {
-
-    strings.sort(function(a,b){
-      var an = a.substr(n,1);
-      var bn = b.substr(n,1);
-      if(an==bn){
-        return a<b ? -1 : a>b ? 1 : 0;
-      }else{
-        return an<bn ? -1 : an>bn ? 1 : 0;
-      }
-    });
-    return strings;
-
-
-    //return strings.sort((a,b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+  // strings.sort(function(a, b){
+  //   const a_char = a.substr(n, 1);
+  //   const b_char = b.substr(n, 1);
+  //
+  //   if(a_char === b_char){
+  //     return a<b ? -1 : (a>b ? 1 : 0);
+  //   }else{
+  //     return a_char < b_char ? -1 : (a_char > b_char ? 1 : 0);
+  //   }
+  // });
+  // return strings;
+  return strings.sort((a,b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }
 console.log(solution(["abn", "aan", "car"],1));
