60d612abf36fc4e793fd1d38793909ffd8400725
jiwon <iamkjw77@naver.com>
2020-10-23 10:34:17 +0900

    refactor: 다른 방법 추가

--- Level1/Q12917.js
+++ Level1/Q12917.js
@@ -1,15 +1,7 @@
 function solution(s) {
-
-    var arr = new Array();
-    var answer= '';
-    arr = s.split('');
-    arr.sort(function(a,b){
-      return a<b ? 1 : a>b ? -1 : 0;
-    });
-    answer = arr.toString().replace(/,/g,'');
-    return answer;
-
-    //return s.split('').sort().reverse().join("");
+  // return s.split('').sort((a,b) =>{
+  //   return a>b ? -1 : (a === b ? 0 : 1);
+  // }).join('');
+  return s.split('').sort().reverse().join('');
 }
-
 console.log(solution('Zaabcdefg')); //gfedcbaaZ 출력
