3db1ceefac0320b6de67d330a8944b2ad5c9dc56
jiwon <iamkjw77@naver.com>
2020-10-26 10:32:19 +0900

    refactor: add new method

--- Level1/Q12918.js
+++ Level1/Q12918.js
@@ -1,34 +1,8 @@
 function solution(s) {
-    /*
-    var cnt = 0;
-    var arr = s.split('');
-    var arrType = new Array();
-    var num = /[0-9]/;
-
-    for(var key in arr){
-      if(arr.length == 4 || arr.length == 6){
-          arrType.push(num.test(arr[key]));
-      }else{
-        return false;
-      }
-    }
-
-    for(var key in arrType){
-      if(arrType[key] === false){
-        return false;
-      }else{
-        cnt += 1;
-      }
-    }
-
-    return cnt == 4 || cnt == 6 ? true : false;
-    */
-
-    var result = false;
-    if((s.length == 4 || s.length == 6) && /^[0-9]+$/.test(s)){
-      result = true;
-    }
-    return result;
-
+  // const regExp = /^[0-9]*$/g;
+  // return regExp.test(s) ? (s.length === 4 || s.length === 6 ? true : false) : false;
+  let result = false;
+  if((s.length === 4 || s.length === 6) && /^[0-9]+$/.test(s)) return true;
+  return result;
 }
-console.log(solution('155505')); //true 출력
+console.log(solution('155511')); //true 출력
