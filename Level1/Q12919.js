cc54fe6119b494921c3ed82b19273a8cf8c005c6
jiwon <iamkjw77@naver.com>
2020-10-27 10:28:53 +0900

    refactor: 변수 선언방식 변경

--- Level1/Q12919.js
+++ Level1/Q12919.js
@@ -1,6 +1,8 @@
 function solution(seoul) {
-    var x = seoul.indexOf('Kim');
-    return '김서방은 '+x+'에 있다';
+    // for(let i=0; i<seoul.length; i++){
+    //   if(seoul[i] === 'Kim') return '김서방은 '+i+'에 있다';
+    // }
+    const kim = seoul.indexOf('Kim');
+    return '김서방은 ' + kim + '에 있다';
 }
-
 console.log(solution(["ji","K","o","Kim"])); //김서방은 3에 있다
