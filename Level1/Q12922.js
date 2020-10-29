b20cb175e2e31ea994200cfa7dc24b3eeb7bebc2
jiwon <iamkjw77@naver.com>
2020-10-29 11:02:57 +0900

    refactor: 다른 방법 풀이

--- Level1/Q12922.js
+++ Level1/Q12922.js
@@ -1,21 +1,9 @@
 function solution(n) {
-/*
-    var answer = '';
-    var a = '수';
-    var b = '박';
-
-    if(n%2 == 1){
-      if(n==1) return answer += a;
-      answer = (a+b).repeat(n/2) + a.repeat(n%2);
-    }else{
-      answer = (a+b).repeat(n/2);
-    }
-
-    return answer;
-*/
-    var word = '수박';
-    return word.repeat(n/2) + (n%2 == 1 ? word.slice(0,1) : '');
+  // if(n%2 === 0) return '수박'.repeat(n/2);
+  // else if(n === 1) return '수';
+  // else return '수박'.repeat(Math.floor(n/2)).concat('수');
+  return '수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
 }
 
 
-console.log(solution(6)); // '수박수박수박' 출력
+console.log(solution(1)); // '수박수박수박' 출력
