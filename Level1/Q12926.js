5cd57df159c1cac56b0eeb087f0bb8c5bbae516b
jiwon <iamkjw77@naver.com>
2020-10-30 11:40:29 +0900

    refactor: 변수 선언 방식 변경

--- Level1/Q12926.js
+++ Level1/Q12926.js
@@ -1,46 +1,20 @@
 function solution(s, n) {
-    /*
-    var answer = '';
-    var arr = s.split('');
-    var a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    var b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    let answer = '';
 
-    for(var i=0;i<arr.length;i++){
-      if(a.indexOf(arr[i]) != -1){
-        if(a.indexOf(arr[i])+n > 25){
-          answer += a[a.indexOf(arr[i])+n-26];
-        }else{
-          answer += a[a.indexOf(arr[i])+n];
-        }
-      }else if(b.indexOf(arr[i]) != -1){
-        if(b.indexOf(arr[i])+n > 25){
-          answer += b[b.indexOf(arr[i])+n-26];
-        }else{
-          answer += b[b.indexOf(arr[i])+n];
-        }
-      }else{
-        answer+= ' ';
-      }
-    }
-    return answer;
-    */
-
-    var answer = '';
-    var lower = 'abcdefghijklmnopqrstuvwxyz';
-    var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    for(var i=0; i<s.length; i++){
-      if(s[i] == ' '){
+    for(let i=0; i<s.length; i++){
+      let text = s[i];
+      if(text === ' '){
         answer += ' ';
         continue;
       }
-      var word = lower.includes(s[i]) ? lower : upper;
-      var index = word.indexOf(s[i])+n;
-      if(index >= word.length) index -= word.length;
-      answer += word[index];
+      const textArr = lower.includes(text) ? lower : upper;
+      let index = textArr.indexOf(text)+n;
+      if(index >= textArr.length) index -= textArr.length;
+      answer += textArr[index];
     }
-
     return answer;
 }
 
-console.log(solution('a c z',25)); //z b y 출력
+console.log(solution('a B z',4)); //e F d 출력
