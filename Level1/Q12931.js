6f75b819cf02a3726de1518966c4d0f497c5f766
jiwon <iamkjw77@naver.com>
2020-11-06 10:59:42 +0900

    refactor: 변수 선언 방식 변경

--- Level1/Q12931.js
+++ Level1/Q12931.js
@@ -1,15 +1,7 @@
 function solution(n)
 {
-    /*
-    var arr = n.toString().split('');
-    var sum = 0;
-    for(var key in arr){
-      sum += parseInt(arr[key]);
-    }
-    return sum;
-    */
-    return (n+'').split('').reduce((acc,curr) => acc+parseInt(curr),0);
-
+    return (n+'').split('').reduce((acc,curr) => acc + parseInt(curr), 0);
+    // return n.toString().split('').reduce((acc, curr) => acc + parseInt(curr), 0);
 }
 
 console.log(solution(1234)); // 10출력
