50a539eb386b336de16fbdc61e51af68f128ff72
jiwon <iamkjw77@naver.com>
2020-11-19 11:48:20 +0900

    feat: 변수 선언 방식 변경

--- Level1/Q12932.js
+++ Level1/Q12932.js
@@ -1,14 +1,11 @@
 function solution(n) {
-    /*
-    var answer = [];
-    var arr = (n+'').split('').reverse();
-    for(var key in arr){
-      answer.push(parseInt(arr[key]));
-    }
-    return answer;
-    */
-    return (n+'').split('').reverse().map(n => parseInt(n));
-    //return (n+'').split('').reverse().map(function(n){return parseInt(n)});
-
+  // return (n+'').split('').reverse().map(v => parseInt(v));
+  // return n.toString().split('').reverse().map(v => parseInt(v));
+  const answer = [];
+  const arr = (n+'').split('').reverse();
+  for(let key in arr){
+    answer.push(parseInt(arr[key]));
+  }
+  return answer;
 }
 console.log(solution(12345)); //[5,4,3,2,1] 출력
